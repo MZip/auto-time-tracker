cd6289d5a743dcd6472f2ef8ed1855d30d50635e
Max Zipfel <max.zipfel@gmx.de>
2021-05-27 09:18:03 +0200

    Adding daily time filter and more details in the charts

--- streamlit-app.py
+++ streamlit-app.py
@@ -16,6 +16,7 @@ tools_and_color_map = {
     "Visual Studio Code": "#3FAAF2",
     "Jupyter Notebook": "#F37726",
     "PowerPoint": "#D04423",
+    "Microsoft Teams": "#464EB8",
     "Break": "#A5D38B",
     "Outlook": "#0072C6",
     "Power BI": "#F1C710",
@@ -58,8 +59,12 @@ date_selection = col5.date_input('')
 # Filter by date
 data = df[df["date"] == date_selection]
 
+# Only keep values after first non-break action
+first_action = data[data["keyword"] != "Break"]["datetime"].min()
+data = data[data["datetime"] >= first_action]
+
 # Calculate tool changes (for Gantt chart)
-data["tool_change"] = (df["keyword"] != df["keyword"].shift(1)).astype(int)
+data["tool_change"] = (data["keyword"] != data["keyword"].shift(1)).astype(int)
 data["tool_change_counter"] = data["tool_change"].cumsum()
 
 # Prepare data for Gantt chart
@@ -73,11 +78,14 @@ gantt_df = gantt_df.sort_values("datetime")
 col3, col4 = st.beta_columns(2)
 
 # Pie Chart
-fig = px.pie(df, values=[1]*len(data), names=df['keyword'], color=df['keyword'],
+fig = px.pie(data, values=[1]*len(data), names=data['keyword'], color=data['keyword'],
              color_discrete_map=tools_and_color_map)
 fig.update_traces(hole=.6, hoverinfo="label+percent")
-fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=-0.2, xanchor="right", x=0.75))
-col3.subheader("Time per application")
+total_screentime = np.round(len(data)/60,1)
+fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=-0.4, xanchor="center", x=0.5),
+                  annotations=[dict(text=f"{total_screentime}h", x=0.5, y=0.5, font_size=40, showarrow=False)]  
+)
+col3.subheader("Total screentime and time per application")
 col3.plotly_chart(fig, config=config)
 
 # Gantt Chart
@@ -87,6 +95,8 @@ fig = px.timeline(gantt_df, x_start="datetime", x_end="datetime_finish", y="keyw
 #fig.update_layout(showlegend=False, plot_bgcolor='rgb(256,256,256)') 
 fig.update_layout(showlegend=False, template="plotly_white")
 fig.update_yaxes(visible=True, showticklabels=True)
-col4.subheader("Timeline")
+first_log = data['datetime'].min().strftime("%H:%M")
+last_log = data['datetime'].max().strftime("%H:%M")
+col4.subheader(f"Timeline per application | {first_log} - {last_log}")
 col4.plotly_chart(fig, config=config)
 
